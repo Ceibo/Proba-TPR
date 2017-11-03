ee390f701cf633af4ef0764dfa3ebe4ec65ec9c7
Luis Fernando Greco <lgreco@dc.uba.ar>
2017-11-02 21:30:20 -0300

    Agrego apunte y codigo del 2)a), la parte del informe

--- TP.r
+++ TP.r
@@ -28,16 +28,27 @@ for (j in 1:3000){
   z[j] <- Generar.exponenciales_devolviendo_media(j, 3)
 }
 
-#Y si hago así?
-z <- seq(length = 3000)
-z <- rexp(3000, rate = 3)
-#Digo, no estaría usando un for
+
 plot(y)
 plot(z)
 #Ver los graficos bizarros que me da
 
 #Ejercicio 2
+#2 a)
+medias <- seq(length=1000)
+for (j in 1:1000){
 x1 <- rexp(1, rate = 3)
 x2 <- rexp(1, rate = 3)
+medias[j] <- (x1+x2)/2
+}
+#Histograma
+hist(medias)
+
+#Boxplot
+boxplot(medias)
+
+#Q-Q Plot
+qqnorm(medias)
+qqline(medias)#La cola del plot
 
 
